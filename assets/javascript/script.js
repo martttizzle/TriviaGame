c27e86d45fb684c4b79da9c4474ee9068caee6c1
Vinod <ramdinv@gmail.com>
2018-04-11 05:39:43 -0500

    jquery fade in and out

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,5 +1,13 @@
 $(document).ready(function () {
 
+    
+    //$("#pic1").append('<img id="yes" src="assets/images/star-wars.jpg">')
+    // $("#yes").fadeOut();
+    
+    $("div.yes").on("click", function() {
+        $(this).fadeOut(5000);
+    });
+    
     var qDiv = $("#questionBox");
     var opt1 = $("#ans1");
     var opt2 = $("#ans2");
@@ -7,41 +15,56 @@ $(document).ready(function () {
     var opt4 = $("#ans4");
     var qcnt = 0;
     var intervalID;
-
+    
     $(document).on('click', function() {
-        start(qcnt);
-        alert(qcnt);
-    });
-   //intervalID = setInterval(start(i), 1000);
-    function start(i) {
-        let quest = question[i].q1;
-        let ans1 = question[i].a;
-        let ans2 = question[i].b;
-        let ans3 = question[i].c;
-        let ans4 = question[i].d;
-
-        qDiv.html("<p>" + quest + "</p>");
-        opt1.html("<p>A." + "  " + ans1 + "</p>");
-        opt2.html("<p>B." + "  " + ans2 + "</p>");
-        opt3.html("<p>C." + "  " + ans3 + "</p>");
-        opt4.html("<p>D." + "  " + ans4 + "</p>");
-        qcnt++; 
-    }
-//Selecting the answer
- $("#ans1, #ans2, #ans3, #ans4").on("click", function() {
-    let userPick = $(this).attr("id");
-    let a = question[qcnt].answer; 
-    if(userPick == a)  {
-        alert("correct");
-        start(qcnt);
-    }
-});
- 
-
-//  $("#tst").on("click", function () {
-//      start(qcnt);
-//  });
-
-
-
-});
+        $("div.yes").fadeOut();
+        });
+        function start(i) {
+            let quest = question[i].q1;
+            let ans1 = question[i].a;
+            let ans2 = question[i].b;
+            let ans3 = question[i].c;
+            let ans4 = question[i].d;
+            
+            qDiv.html("<p>" + quest + "</p>");
+            opt1.html("<p>A." + "  " + ans1 + "</p>");
+            opt2.html("<p>B." + "  " + ans2 + "</p>");
+            opt3.html("<p>C." + "  " + ans3 + "</p>");
+            opt4.html("<p>D." + "  " + ans4 + "</p>");
+            qcnt++; 
+        }
+        //Selecting the answer
+        $("#ans1, #ans2, #ans3, #ans4").on("click", function() {
+            let userPick = $(this).attr("id");
+            let a = question[qcnt].answer; 
+            if(userPick == a)  {
+                alert("correct");
+                start(qcnt);
+            }
+        });
+        
+        
+        //  $("#tst").on("click", function () {
+            //           $("#tst").append('<p id="yes">OK </p>');
+            
+            //         });
+            
+            // var testCounter = 0;
+            // function test() {
+                //       testCounter += 1;
+                //     console.log ("test " + testCounter);
+                
+                // }
+                
+                //setInterval(test, 1000);
+                
+                //intervalID = setInterval(start(i), 1000);
+                
+                
+                
+                // $("#body").toggle();
+                
+                
+                
+                
+            });
