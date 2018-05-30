b903d98a331a49c481a4d6586129cf6aee5dc92d
Vinod <ramdinv@gmail.com>
2018-05-30 17:12:37 -0500

    major update: working game; added in graphics;need to work on mobile responsive

--- assets/javascript/questions.js
+++ assets/javascript/questions.js
@@ -5,70 +5,86 @@ var question = [{
     c: "Darth Vapor",
     d: "Lando Calrissian",
     answer: "ans2"
-},{
+}, {
     q1: "Fill in the blank: \"Help me, _________. You're are my only hope.\"",
     a: "Rey",
     b: "Leia Organa",
     c: "Obi-Wan Kenobi",
     d: "General Hux",
     answer: "ans3"
-},{
+}, {
     q1: "What is the name of Boba Fett's ship?",
     a: "Slave 1",
     b: "Millennium Falcon",
     c: "Tantive IV",
     d: "IG-2000",
     answer: "ans1"
-},{
+}, {
     q1: "Han Solo's trusty blaster is named:",
     a: "Solo-6466",
     b: "DL-44",
     c: "laser-beam6",
     d: "LP0-1250",
-    answer:"ans2"
-},{
+    answer: "ans2"
+}, {
     q1: "How many parsecs did the Millennium Falcon make the Kessel Run in?",
     a: "Less than 12 parsecs",
     b: "Less than 10 parsecs",
     c: "Less than 20 parsecs",
     d: "It was measured in Kessels",
     answer: "ans1"
-},{
+}, {
     q1: "What is the name of Luke Skywalkers X-wing?",
     a: "Red Leader",
     b: "Red 2",
     c: "Red 5",
     d: "Red 0",
     answer: "ans3"
-},{
+}, {
     q1: "You may know them by AT-AT, but what does it stand for?",
     a: "All Terrain Armored Transport",
     b: "All Terrain Assualt Transport",
     c: "All Terrain Attack Transport",
     d: "All Tactical Armored Tank",
     answer: "ans1"
-},{
+}, {
     q1: "What part of the second Death Star was targeted by Lando and Wedge in order to destroy it entirely?",
     a: "Electromatic Shield-capacitor",
     b: "Thermal II exhaust port",
     c: "Hypermatter Core",
     d: "Sheild Generator",
-    answer:"ans3"
-},{
+    answer: "ans3"
+}, {
     q1: "Which Imperial Admiral did Vader force-choke for coming out of the hyperspace too close to Hoth, and alerting the Rebels?",
     a: "Admiral Ozzel",
     b: "Admiral Needa",
     c: "Admiral Veers",
     d: "Admiral Piett",
     answer: "ans1"
-},{ 
+}, {
     q1: "In what \"Detention Block\" was Princess Leia being held on the first Death Star?",
     a: "AA-23",
     b: "AA-24",
     c: "C-block 2138",
     d: "C-block 2343",
-    answer:"ans1"
+    answer: "ans1"
 },
 {}
 ];
-
+// Gifs
+var vader = '<img id="pic" class="col-xs-6" src="assets/images/vader.gif">';
+var leiaOrgana = '<img id="pic" src="assets/images/leiaOrgana.gif">';
+var slave1 = '<img id="pic" src="assets/images/slave1.gif">';
+var DL44 = '<img id="pic" src="assets/images/DL-44.gif">';
+var Kessel = '<img id="pic" src="assets/images/kessels.gif">';
+var Red5 = '<img id="pic" src="assets/images/red5.gif">';
+var ATAT = '<img id="pic" src="assets/images/atat.gif">';
+var Deathstar = '<img id="pic" src="assets/images/deathstarblow2.gif">';
+var Ozzel = '<img id="pic" src="assets/images/ozzel.gif">';
+var Cblock = '<img id="pic" src="assets/images/cellblock.gif">';
+//GIF array
+var gifClip = [vader, leiaOrgana, slave1, DL44, Kessel, Red5, ATAT, Deathstar, Ozzel, Cblock];
+// array of wrong answers
+var wrongAnswerArry = [question[0].b, question[1].c, question[2].a, question[3].b, question[4].a, question[5].c, question[6].a, question[7].c, question[8].a, question[9].a];
+//Background images
+var bkgrdImg = ["assets/images/lukejpg.jpg","assets/images/leiajpg.jpg","assets/images/slave1png.png","assets/images/hansolojpg.jpg","assets/images/milleneniumjpg.jpg","assets/images/red5jpg.jpg","assets/images/atatpng.png","assets/images/deathstar2jpeg.jpg","assets/images/detentionblockjpg.jpg",];
--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -9,61 +9,58 @@ $(document).ready(function () {
     var crtResults = 0; //for endslide correct count
     var wrgResults = 0; // for endslide wrong count
     var noAnswer = 0; // for endslide no answer count
-    var counter = 0;
-    var timeleft = 11;
+    var timeLeft = 10;
     var intervalID;
     var startButtonTimer
     var correct;
     var button;
-    var bkgrdImg = "../images/deathstar.jpg";
-    //right or wrong answer gifs
-    var vader = '<img id="pic" class="col-xs-6" src="assets/images/vader.gif">';
-    var leiaOrgana = '<img id="pic" src="assets/images/leiaOrgana.gif">';
-    var slave1 = '<img id="pic" src="assets/images/slave1.gif">';
-    var DL44 = '<img id="pic" src="assets/images/DL-44.gif">';
-    var Kessel = '<img id="pic" src="assets/images/kessels.gif">';
-    var Red5 = '<img id="pic" src="assets/images/red5.gif">';
-    var ATAT = '<img id="pic" src="assets/images/atat.gif">';
-    var Deathstar = '<img id="pic" src="assets/images/deathstarblow2.gif">';
-    var Ozzel = '<img id="pic" src="assets/images/ozzel.gif">';
-    var Cblock = '<img id="pic" src="assets/images/cellblock.gif">';
-    //image array
-    var gifClip = [vader, leiaOrgana, slave1, DL44, Kessel, Red5, ATAT, Deathstar, Ozzel, Cblock];
-    // array of wrong answers
-    var wrongAnswerArry = [question[0].b, question[1].c, question[2].a, question[3].b, question[4].a, question[5].c, question[6].a, question[7].c, question[8].a, question[9].a];
-
-
-
-
-
-
-
-
-
-
-
+  
     //Click to start and timer start
-    // debugger;
+
+    
+    $("#strBtn").fadeIn(5000);
+    $(".victory").fadeIn(2500);
+    // $("#strBtn").fadeOut(5000);
     $(".container").hide();
     $("#strBtn").on("click", function () {
         $(this).hide();
+        $(".victory").hide();
         $("#yes").fadeOut(500);
         $(".container").show();
+        $("#timer").html("<b>" + "Time Remaining: " + timeLeft + " Seconds " + "</b>");
         start(qcnt);
     });
 
-
     function start(qcnt) {
-        clearInterval(intervalID);
-    if (qcnt <  10) {
-        questions(qcnt);
-    }else{
-        gameoverSlide();
+        if (qcnt <  10) {
+            clearInterval(intervalID);
+           backgroundChange(qcnt);
+            questions(qcnt);
+            timeIt(10);
+        }
+        else{
+            clearInterval(intervalID);
+            gameoverSlide();
+        }
     }
         
-     
+    function timeIt(timeLeft) {
+    intervalID = setInterval(function() { 
+        if (timeLeft > 1) {
+            $("#timer").html("<b>" + "Time Remaining: " + timeLeft + " Seconds " + "</b>");
+        }
+        else {
+            $("#timer").html("<b>" + "Time Remaining: " + timeLeft + " Second " + "</b>");
+        }
+        timeLeft--;
+
+        if (timeLeft < 0) {
+            clearInterval(intervalID); // Clears the setInterval for timer                 
+             noAnswer++;
+              setTimeout(imageResults(qcnt, false), 5000);//timer not working
+        }
+    }, 1000);           
 }
-    // intervalID = setInterval(timeIt, 1000);
     //questions generated
     function questions(i) {
         console.log(i,"the question reset");
@@ -76,112 +73,87 @@ $(document).ready(function () {
         opt1.html("<p>A. " + "  " + ans1 + "</p>");
         opt2.html("<p>B. " + "  " + ans2 + "</p>");
         opt3.html("<p>C. " + "  " + ans3 + "</p>");
-        opt4.html("<p>D. " + "  " + ans4 + "</p>");   
-        intervalID = setInterval(timeIt, 1000); 
-
+        opt4.html("<p>D. " + "  " + ans4 + "</p>");    
 }
 
     //answer clicked
     $("#ans1, #ans2, #ans3, #ans4").on("click", function () {
+
+        clearInterval(intervalID);
         let userPick = $(this).attr("id");
         let ans = question[qcnt].answer;
 
         if (userPick == ans) {
             correct = true;
             crtResults += 1;
-            clearInterval(intervalID);
             imageResults(qcnt, correct);
         } else {
             correct = false;
             wrgResults += 1;
-            clearInterval(intervalID);
             imageResults(qcnt, correct);
         }
     });
 
     //answer images
     function imageResults(qcnt, correct) {
-        clearInterval(intervalID);
-        //hiding the question and answer box 
+              //hiding the question and answer box 
         $("#ans-image, #questionBox").hide();
-        //showing the gif clip
+             //showing the gif clip
         $("#ok").append(gifClip[qcnt]).show();
-        //Conditional statements allow the right or wrong text to be displayed
+            //Conditional statements allow the right or wrong text to be displayed
         if (correct == true) {
-            $("#answer").append("<p>CORRECT!</p>").show();
+            $("#answer").append("<p id='answer'>CORRECT!</p>").show();
         }
         else if (correct == false) {
             $("#answer").append("<p id='answer'>" + "WRONG! " + "<br>" + ' correct answer: ' +
             wrongAnswer(qcnt) + "</p>").show();
         } 
-        //Gif display length 
-        setTimeout(showResults, 3000);//***********************************************************************************************************/
+           //Gif display length 
+        setTimeout(showResults, 4500);
      
     }
-    //Places questionbox back on screen and empties gif element and right or wrong text
+          //Places questionbox back on screen and empties gif element and right or wrong text
     function showResults() {
+        clearInterval(intervalID);
         $("#ans-image, #questionBox").show();
         $("#ok, #answer").empty();
-        counter = 0;
-        qcnt++;
-        start(qcnt);
+        qcnt++
+        timeLeft = 10;
+        $("#timer").html("<b>" + "Time Remaining: " + timeLeft + " Seconds " + "</b>");
+        start(qcnt); 
     }
 
     function wrongAnswer(qcnt) {
         var wrgAnsTxt = wrongAnswerArry[qcnt];
         return wrgAnsTxt;
     }
-
-
-
-    function timeIt() {
-        counter++;
-        let countdown = (timeleft - counter);
-        $("#timer").html("<b>" + "Time Remaining: " + countdown + " seconds " + "</b>");
-        
-        if (countdown == 0) {
-            clearInterval(intervalID);
-            counter = 0;
-            noAnswer++ 
-            console.log(noAnswer, "------------------------------------no answer counter");
-            imageResults(qcnt, false);
-            qcnt++
-            start(qcnt);
-        }
+ 
+    function backgroundChange(num) {
+        $("#body,.jumbotron").css("background-image","url("+bkgrdImg[num]+")").fadeOut(0);
+        $("#body,.jumbotron").css("background-image","url("+bkgrdImg[num]+")").fadeIn(1500);
+       
     }
-
-    //Display the correct answer after wrong selection
-
-
-    // function backgroundChange() {
-    //     $("#body").css('background-image', 'url("' + bkgrdImg + '")');
-    // }
-    //I was trying to make the button call this function when clicked on as an in-line call
-    //but it wouldn't mak a call, no matter how I tried with it. 
-
-
-    function gameoverSlide(i) {
-
-        $("#ans-image, #questionBox").hide();
-
-        $("#ok").append("<p id='endAns'>" + "GAME OVER" + "<br>" + "Correct Answer:" +
+    function gameoverSlide() {
+        clearInterval(intervalID);
+        $("#ans-image, #questionBox, #ok").hide();
+        $("#results").append("<p id='endAns'>" + "GAME OVER" + "<br>" + "Correct Answer:" +
             crtResults + "<br>" + "Wrong Answers:" + wrgResults + "<br>" + "No Answer:" + noAnswer + "</p>").show();
-
-        button = $("#ok").prepend("<button id='button'>" + "START OVER?" + "</button>").show();
+        button = $("#bttn").prepend("<button id='button'>" + "START OVER?" + "</button>").show();
 
         $(button).on("click", function () {
-            clearInterval(intervalID);/***********************************************************************************************************/
+            clearInterval(intervalID);
             crtResults = 0;
             wrgResults = 0;
             noAnswer = 0;
             qcnt = 0;
-            $("#ok").append("<p id='endAns'>" + "GAME OVER" + "<br>" + "Correct Answer:" +
+            $("#results").append("<p id='endAns'>" + "GAME OVER" + "<br>" + "Correct Answer:" +
             crtResults + "<br>" + "Wrong Answers:" + wrgResults + "<br>" + "No Answer:" + noAnswer + "</p>").empty();
 
-            button = $("#ok").prepend("<button id='button'>" + "START OVER?" + "</button>").empty();
-            $("#ans-image, #questionBox").show();
+            button = $("#bttn").prepend("<button id='button'>" + "START OVER?" + "</button>").empty();
 
-            questions(qcnt);
+            $("#ans-image, #questionBox").show();
+            timeLeft = 10;
+            start(qcnt);
 
         });
 
